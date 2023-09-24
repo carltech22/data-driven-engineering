1174f00c5a1523ba18482ea8778e2080512510fb
Carlos Zuna Largo <carlos.zunalargo@outlook.com>
2023-09-24 13:21:56 -0700

    My first project file on SVD

---
+++ chapter1/SVD/compute_svd.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# Create random data Matrix
+X = np.array([[1,2],[3,4],[5,6],[7,8]])
+print("Matrix X = size" + str(X.shape))
+print(X)
+
+# Full SVD
+print("**** Full SVD ****")
+
+U,S,VT = np.linalg.svd(X,full_matrices=True) 
+print("Matrix U = size" + str(U.shape))
+print(U)
+
+print("Matrix S = " + str(S.shape))
+print(S)
+
+print("Matrix VT = size" + str(VT.shape))
+print(VT)
+
+#Economy SVD
+print("**** Economy SVD ****")
+
+Uhat, Shat, VThat = np.linalg.svd(X,full_matrices=False) 
+print("Matrix U = size" + str(Uhat.shape))
+print(Uhat)
+
+print("Matrix S = " + str(Shat.shape))
+print(Shat)
+
+print("Matrix VT = size" + str(VThat.shape))
+print(VThat)
